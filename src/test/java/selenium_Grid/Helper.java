2aee4c4db9eea9168df6f17977cabf00fbe99aba
Shivangi020 <me.shivasingh.0987@gmail.com>
2023-07-22 14:57:15 +0530

    setting up selenium grid initialize browser function

---
+++ src/test/java/selenium_Grid/Helper.java
@@ -0,0 +1,35 @@
+package selenium_Grid;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class Helper {
+	
+	public static WebDriver initializeBrowser(String browserName) throws MalformedURLException {
+		
+		WebDriver driver;
+		
+		DesiredCapabilities dc = new DesiredCapabilities();
+		if(browserName.equals("chrome")) {
+			dc.setBrowserName("chrome");
+		}else if(browserName.equals("firefox")) {
+			dc.setBrowserName("firefox");
+		}else if(browserName.equals("edge")) {
+			dc.setBrowserName("MicrosoftEdge");
+		}else if(browserName.equals("ie")) {
+			dc.setBrowserName("internet explorer");
+		}else if(browserName.equals("opera")) {
+			dc.setBrowserName("opera");
+		}else if(browserName.equals("safari")) {
+			dc.setBrowserName("safari");
+		}
+
+		driver = new RemoteWebDriver(new URL("http://localhost:4444") ,dc);
+		return driver;
+	}
+
+}
--- src/test/java/selenium_Grid/Test2.java
+++ src/test/java/selenium_Grid/Test2.java
@@ -7,19 +7,19 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class Test1 {
+public class Test2 {
 	
 	public WebDriver driver;
 	
 	@Test
-    public void testOne() {
+    public void testTwo() {
 		driver.get("https://the-internet.herokuapp.com/");
-		System.out.println(driver.getTitle()+" from chrome browser");
+		System.out.println(driver.getTitle()+" from firefox");
 	}
 	
 	@BeforeMethod
 	public void setup() throws MalformedURLException {
-		driver = Helper.initializeBrowser("chrome");
+		driver = Helper.initializeBrowser("firefox");
 		
 	}
 	
